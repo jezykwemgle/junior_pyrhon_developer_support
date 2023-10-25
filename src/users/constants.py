0d7f2ca6035b6f7ebe290d598a86eef7dac80cf7
jezyk_we_mgle <anastasiafabricnaama@gmail.com>
2023-10-25 18:34:03 +0300

    new model user

---
+++ src/users/constants.py
@@ -0,0 +1,14 @@
+from enum import StrEnum
+
+
+class Role(StrEnum):
+    ADMIN = "AD"
+    JUNIOR = "JR"
+    SENIOR = "SR"
+
+    def values(cls):
+        results = []
+        for element in cls:
+            el = (element.value, element.value)
+            results.append(el)
+        return results
--- src/users/models.py
+++ src/users/models.py
@@ -1,21 +1,46 @@
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
 from django.db import models
 
-
-class Role(models.Model):
-    value = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.value
+from users.constants import Role
+from users.managers import UserManager
 
 
-class User(models.Model):
-    email = models.EmailField(unique=True)
+class User(AbstractUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    role = models.IntegerField()
+
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    role = models.CharField(
+        max_length=2, default=Role.JUNIOR
+    )  # TODO: constants choices=Role.values()
+
+    objects = UserManager()
+
+    EMAIL_FIELD = (
+        "email"  # email = models.EmailField(max_length=255, unique=True)
+    )
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def get_full_name(self):
+        """
+        Return the first_name plus the last_name, with a space in between.
+        """
+        full_name = "%s %s" % (self.first_name, self.last_name)
+        return full_name.strip()
+
+    def get_short_name(self):
+        """Return the short name for the user."""
+        return self.first_name
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        if self.first_name and self.last_name:
+            return f"{self.first_name} {self.last_name}"
+        else:
+            return self.email
 
 
 # class Role(models.Model):
