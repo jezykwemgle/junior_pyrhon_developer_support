bc5e6d5cab6149f7964d06b31244f07e9212842a
jezyk_we_mgle <anastasiafabricnaama@gmail.com>
2023-11-02 22:09:07 +0200

    updates

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,13 +1,10 @@
 from django.contrib import admin
-from django.urls import path
-
-from issues import api as issue_api
-from users import api as user_api
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('users/all/', all_users),
-    path('users/create/', user_api.create),
-    # path('issues/all/', all_issues),
-    path('issues/create/', issue_api.create)
+    path('api-auth/', include('rest_framework.urls')),
+    path('users/', include('users.urls')),
+    path("auth/", include("authentication.urls")),
+    path("issues/", include("issues.urls"))
 ]
