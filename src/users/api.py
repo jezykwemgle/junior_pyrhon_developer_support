a27c760b626eb7332fa4d8207f3f5e6eab498e7e
jezyk_we_mgle <anastasiafabricnaama@gmail.com>
2023-11-03 12:35:50 +0200

    fix UserAPISet

--- src/users/api.py
+++ src/users/api.py
@@ -7,19 +7,15 @@ from .serializers import UserPrivateSerializer, UserPublicSerializer
 
 class UserAPISet(ModelViewSet):
     queryset = User.objects.all()
-    serializer_class = UserPublicSerializer
-    permission_classes = [permissions.AllowAny]
 
     def get_permissions(self):
         if self.action == "create":
             return [permissions.AllowAny()]
         elif self.action == "list" or "retrieve":
             return [permissions.IsAuthenticated()]
-        return self.permission_classes
 
     def get_serializer_class(self):
         if self.action == "create":
             return UserPrivateSerializer
         elif self.action == "list" or self.action == "retrieve":
-            return self.serializer_class
-        return self.serializer_class
+            return UserPublicSerializer
